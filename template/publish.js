193febdcc51c6db0dc2f22a76407510f0ac391e0
Sir Robert Burbridge <sirrobert@gmail.com>
2020-07-12 01:34:30 -0400

    Add support for configs and other improvements.

--- template/publish.js
+++ template/publish.js
@@ -1,7 +1,10 @@
-const fs   = require('fs');
+const fs       = require('fs');
+const path     = require('path');
+const jsonfile = require('jsonfile');
 
 /**
- * Publish JSDoc data as JSON data.
+ * Publish JSDoc data as JSON data.  In this template, command line takes
+ * priority over config and config takes priority over default.
  *
  * @param jsdocs {TaffyDB} A Taffy DB data object representing the JSDoc
  * data.
@@ -10,15 +13,43 @@ const fs   = require('fs');
  * @returns {Promise} A promise object that accepts when the file is written
  * as expected and rejects when there's an error creating the JSON file.
  */
-function publish (jsdocs, options) {
+function publish (jsdocs, options, tutorials) {
+  let fn_dest  = 'jsdoc-data.json';
+  let config   = {};
+  let defaults = {
+    debug       : false,
+    config      : "conf.json",
+    destination : "./out",
+    readme      : "README.md",
+  };
+
+  if (options && typeof options.configure === "string") {
+    // I know, I know.  Sync is bad mojo.  But this JSDoc template isn't in
+    // a hurry, so ... fork it and write your own if you want it async =)
+    config = jsonfile.readFileSync(
+      path.normalize(process.cwd() + '/' + options.configure)
+    );
+  }
+
+  // We merge the three objects into a new parameters.
+  //    <-- Defaults <-- config <-- cmdline
+  let params = Object.assign({}, defaults, config, options);
+
+  // In the params, we want to normalize the output directory.
+  if (options && typeof options.destination === "string") {
+    fn_dest = path.normalize(params.destination + "/" + fn_dest);
+  }
+
   const doclets = [];
   jsdocs().each(d => { doclets.push(d) });
 
-  fs.writeFile(
-    'jsdoc.json',
-    JSON.stringify(doclets),
-    console.log
-  );
+  fs.writeFile(fn_dest, JSON.stringify(doclets), err => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("JSDoc data written to", fn_dest);
+    }
+  });
 }
 
 module.exports = {
--- test/template.test.js
+++ test/template.test.js
@@ -1,7 +1,13 @@
 const fs       = require('fs');
+const jsonfile = require('jsonfile');
+const path     = require('path');
 const template = require('../template/publish.js');
 
 describe("template", () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
   test("publish()", () => {
     fs.writeFile = jest.fn();
 
@@ -18,10 +24,94 @@ describe("template", () => {
     });
 
     expect(fs.writeFile.mock.calls.length).toBe(1);
-    expect(fs.writeFile.mock.calls[0][0]).toBe('jsdoc.json');
+    expect(fs.writeFile.mock.calls[0][0]).toBe('jsdoc-data.json');
     expect(fs.writeFile.mock.calls[0][1]).toBe(
       '[{"one":1,"two":2},{"three":3,"four":4},{"five":5,"six":6}]',
     );
   });
-});
 
+  test("configure file", () => {
+    jsonfile.readFileSync = jest.fn();
+    fs.writeFile = jest.fn((fxn, txt, cb) => {
+      cb("File writing error");
+    });
+    console.error = jest.fn();
+
+    template.publish(
+      function () {
+        return {
+          each: function (func) {
+            [
+              { one   : 1, two  : 2 },
+              { three : 3, four : 4 },
+              { five  : 5, six  : 6 },
+            ].forEach(d => func(d));
+          }
+        };
+      },
+      // These are the options, like the command line options.
+      {
+        configure   : 'myconfig.json',
+        destination : 'pub',
+      }
+    );
+
+    // Test the config file
+    expect(jsonfile.readFileSync.mock.calls.length).toBe(1);
+    expect(
+      path.relative(process.cwd(), jsonfile.readFileSync.mock.calls[0][0])
+    ).toBe('myconfig.json');
+
+    // Test the "destination" element of the config.
+    expect(fs.writeFile.mock.calls.length).toBe(1);
+    expect(fs.writeFile.mock.calls[0][0]).toBe('pub/jsdoc-data.json');
+
+    // Test that we got the expected console error.
+    expect(console.error.mock.calls.length).toBe(1);
+    expect(console.error.mock.calls[0][0]).toBe('File writing error');
+  });
+
+  test("configure file", () => {
+    console.log   = jest.fn();
+    console.error = jest.fn();
+    jsonfile.readFileSync = jest.fn();
+    fs.writeFile = jest.fn((fxn, txt, cb) => {
+      cb();
+    });
+
+    template.publish(
+      function () {
+        return {
+          each: function (func) {
+            [
+              { one   : 1, two  : 2 },
+              { three : 3, four : 4 },
+              { five  : 5, six  : 6 },
+            ].forEach(d => func(d));
+          }
+        };
+      },
+      // These are the options, like the command line options.
+      {
+        configure   : 'myconfig.json',
+        destination : 'pub',
+      }
+    );
+
+    // Test the config file
+    expect(jsonfile.readFileSync.mock.calls.length).toBe(1);
+    expect(
+      path.relative(process.cwd(), jsonfile.readFileSync.mock.calls[0][0])
+    ).toBe('myconfig.json');
+
+    // Test the "destination" element of the config.
+    expect(fs.writeFile.mock.calls.length).toBe(1);
+    expect(fs.writeFile.mock.calls[0][0]).toBe('pub/jsdoc-data.json');
+
+    // Test when file writing succeeds.  We don't get a filename here
+    // because we're not using the internal enclosure in the module.  I
+    // don't know how to test that with jest yet.
+    expect(console.log.mock.calls.length).toBe(1);
+    expect(console.log.mock.calls[0][0]).toBe('JSDoc data written to');
+  });
+});
